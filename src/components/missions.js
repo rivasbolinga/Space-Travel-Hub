03e8698d660a59de463b40273b5275154ea75434
Andrea <andrearivas4cinfor@gmail.com>
2023-03-14 11:04:23 +0100

    display data

--- src/components/missions.js
+++ src/components/missions.js
@@ -11,12 +11,27 @@ const Missions = () => {
   }, [dispatch]);
 
   return (
-    <>
-      {missions.map((mission) => (
-        <p key={mission.mission_id} className="missionName">{mission.description}</p>
-      ))}
-      ;
-    </>
+    <section className="missions-section">
+      <h2 className="missions-title">Missions</h2>
+      <table className="table-container">
+        <thread>
+          <tr className="mission-tr">
+            <th className="column-title">Missions</th>
+            <th className="column-title">Description</th>
+            <th className="column-title">Status</th>
+          </tr>
+        </thread>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id} className="missionName">
+              <td className="mission-description">{mission.description}</td>
+              <td className="mission-name">{mission.mission_name}</td>
+              <td className="mission-status">Not booked</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 };
 
