d21636fcbc12b18f5ba2c484a4010a515f967aa3
Andrea <andrearivas4cinfor@gmail.com>
2023-03-14 16:23:04 +0100

    Add reducer to join mission

--- src/redux/Missions/missionsSlice.js
+++ src/redux/Missions/missionsSlice.js
@@ -1,22 +1,34 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const fetchMissions = createAsyncThunk(
-  'missions/fetchMissions',
+  "missions/fetchMissions",
   async () => {
-    const response = await axios.get('https://api.spacexdata.com/v3/missions');
+    const response = await axios.get("https://api.spacexdata.com/v3/missions");
     return response.data;
-  },
+  }
 );
 
 const missionsSlice = createSlice({
-  name: 'missions',
+  name: "missions",
   initialState: [],
-  reducers: {},
+  reducers: {
+    joinMission: (state, action) => {
+      const { id } = action.payload;
+      return state.map((mission) => {
+        if (mission.mission_id !== id) {
+          return mission;
+        }
+        return { ...mission, reserved: true };
+      });
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchMissions.fulfilled, (state, action) => action.payload);
   },
 });
+
+export const { joinMission } = missionsSlice.actions;
 
 export const selectMissions = (state) => state.missions;
 
