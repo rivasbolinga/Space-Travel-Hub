baf3b4452aada2d04c14229a53df474553c45d45
Bigizi <bigizinduwayo@gmail.com>
2023-03-14 12:19:08 +0200

    Fetch dragons from API

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 import './navigation.css';
 
 function Navigation() {
   return (
     <nav>
       <ul className="links">
-        <li><NavLink to="/rockets" className="lists">Rockets</NavLink></li>
-        <li><NavLink to="/dragons" className="lists">Dragons</NavLink></li>
-        <li><NavLink to="/missions" className="lists">Missions</NavLink></li>
+        <li><NavLink to="/" className="lists">Rockets</NavLink></li>
+        <li><NavLink to="dragons" className="lists">Dragons</NavLink></li>
+        <li><NavLink to="missions" className="lists">Missions</NavLink></li>
         <li><NavLink to="profile" className="lists">My Profile</NavLink></li>
       </ul>
+      <Outlet />
     </nav>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './Rockets/rocketSlice';
+import dragonReducer from './Dragons/dragonSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketReducer,
+    dragons: dragonReducer,
   },
 });
 
