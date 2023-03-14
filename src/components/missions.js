5f696cf6d7267bd343466eb7a73000eca07e3090
Andrea <andrearivas4cinfor@gmail.com>
2023-03-14 10:57:15 +0100

    fetch data

--- src/components/missions.js
+++ src/components/missions.js
@@ -1,6 +1,23 @@
-const Missions = () => (
-  <h1>Here the missions </h1>
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions, selectMissions } from '../redux/Missions/missionsSlice';
 
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector(selectMissions);
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  return (
+    <>
+      {missions.map((mission) => (
+        <p key={mission.mission_id} className="missionName">{mission.description}</p>
+      ))}
+      ;
+    </>
+  );
+};
 
 export default Missions;
