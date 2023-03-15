79f7d311651ea6d58b9764d1cd865cce13872d91
djo1975 <mladanilic1975@gmail.com>
2023-03-15 08:18:06 +0100

    Implemented logo and title in navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import './navigation.css';
+import planetLogo from '../images/planet.png';
 
 function Navigation() {
   return (
     <nav>
-      <ul className="links">
-        <li><NavLink to="/" className="lists">Rockets</NavLink></li>
-        <li><NavLink to="dragons" className="lists">Dragons</NavLink></li>
-        <li><NavLink to="missions" className="lists">Missions</NavLink></li>
-        <li><NavLink to="profile" className="lists">My Profile</NavLink></li>
-      </ul>
+      <div className="logo">
+        <img src={planetLogo} alt="Space Travel Hub logo" />
+        <h1>Space Travel Hub</h1>
+        <ul className="links">
+          <li><NavLink to="/" className="lists">Rockets</NavLink></li>
+          <li><NavLink to="dragons" className="lists">Dragons</NavLink></li>
+          <li><NavLink to="missions" className="lists">Missions</NavLink></li>
+          <li><NavLink to="profile" className="lists">My Profile</NavLink></li>
+        </ul>
+      </div>
       <Outlet />
     </nav>
   );
 }
-
 export default Navigation;
