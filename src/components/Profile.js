5e8bbf58b834208c7dd4e7afee3a9556daaf48d3
djo1975 <mladanilic1975@gmail.com>
2023-03-15 10:36:02 +0100

    Added Reserved rockets list in my profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAllRockets } from '../redux/Rockets/rocketSlice';
 
 function Profile() {
+  const reservedRockets = useSelector(selectAllRockets).filter((rocket) => rocket.isReserved);
+
   return (
     <>
-      <h1>This my profile</h1>
+      <h2>My Rockets:</h2>
+      {reservedRockets.map((rocket) => (
+        <div key={rocket.id}>
+          <h3>{rocket.name}</h3>
+        </div>
+      ))}
     </>
   );
 }
