44eac5ee8b5584967167d71501d61094f398e7ec
djo1975 <mladanilic1975@gmail.com>
2023-03-15 06:04:42 +0100

    Addedfuncionality for reservations

--- src/redux/Rockets/rocketSlice.js
+++ src/redux/Rockets/rocketSlice.js
@@ -1,21 +1,54 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk, createSelector } from '@reduxjs/toolkit';
+
+const initialState = { rockets: [], toFetch: 'true' };
 
 export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
-  const response = await axios.get('https://api.spacexdata.com/v4/rockets');
-  return response.data;
+  const response = await fetch('https://api.spacexdata.com/v3/rockets');
+  return response.json();
 });
 
-const rocketSlice = createSlice({
+const rocketsSlice = createSlice({
   name: 'rockets',
-  initialState: [],
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchRockets.fulfilled, (state, action) => action.payload);
+  initialState,
+  reducers: {
+    bookRockets: (state, { payload }) => {
+      const newArr = state.rockets.map((rockObj) => {
+        if (rockObj.id === payload) {
+          if (rockObj.isReserved === true) {
+            return { ...rockObj, isReserved: false };
+          }
+          return { ...rockObj, isReserved: true };
+        }
+        return ({ ...rockObj });
+      });
+      const newState = { ...state };
+      return { ...newState, rockets: newArr };
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchRockets.fulfilled, (state, action) => {
+      const newState = { ...state };
+      const rocketsCard = [];
+      action.payload.forEach((rockObj) => {
+        rocketsCard.push({
+          id: rockObj.rocket_id,
+          name: rockObj.rocket_name,
+          description: rockObj.description,
+          image: rockObj.flickr_images[0],
+          isReserved: false,
+        });
+      });
+      newState.rockets = rocketsCard;
+      newState.toFetch = false;
+      return newState;
+    });
   },
 });
 
-export const selectRockets = (state) => state.rockets;
+export const selectAllRockets = createSelector(
+  (state) => state.rockets.rockets,
+  (rockets) => rockets,
+);
 
-export default rocketSlice.reducer;
+export default rocketsSlice.reducer;
+export const { bookRockets } = rocketsSlice.actions;
